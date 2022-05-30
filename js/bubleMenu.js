77ab24e7a0c57f08e8822c028e006432d561861c
Oscar <93570814+Oscar-2283@users.noreply.github.com>
2022-05-30 13:58:38 +0800

    bubleMenu scroll lock

--- js/bubleMenu.js
+++ js/bubleMenu.js
@@ -3,10 +3,28 @@ const menuItems = document.querySelector('#overlay');
 const menuContainer = document.querySelector('.menuContainer');
 const menuIcon = document.querySelector('.bubble-menu-icon');
 const bubleMenu = document.querySelector('.bubleMenu');
-
+const menuLock = document.querySelector('.overlay-menu-ul')
+console.log(menuLock)
 
 
 function toggleMenu(e) {
+
+
+    menuLock.addEventListener('mouseleave',function(){
+        if ($(document).height() > $(window).height()) {
+            $("html").addClass("noscroll");
+          }else{
+            $("html").addClass("fixWindow"); 
+          }
+    })
+    menuLock.addEventListener('click',function(){
+        if ($(document).height() > $(window).height()) {
+            $("html").removeClass("noscroll");
+        }else{
+           $("html").removeClass("fixWindow");
+       }
+    })
+
     menuItems.classList.toggle('open');
     menuContainer.classList.toggle('full-menu');
     menuIcon.classList.toggle('fa-bars');
@@ -17,11 +35,25 @@ function toggleMenu(e) {
     if (menuIcon.classList == 'fa bubble-menu-icon fa-times') {
         bubleMenu.classList.add('w-100')
         bubleMenu.classList.add('h-100')
+        if ($(document).height() > $(window).height()) {
+            $("html").addClass("noscroll");
+          }else{
+            $("html").addClass("fixWindow"); 
+          }
     }else{
         bubleMenu.classList.remove('w-100')
         bubleMenu.classList.remove('h-100')
+        if ($(document).height() > $(window).height()) {
+            $("html").removeClass("noscroll");
+        }else{
+           $("html").removeClass("fixWindow");
+       }
     }
 }
+
+
+
+
 
 
 menuPhone.addEventListener('click', toggleMenu, false);
